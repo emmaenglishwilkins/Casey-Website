d0b55b33ab423a21a70aea402ada79cfaaa8abee
emmaenglishwilkins <78217522+emmaenglishwilkins@users.noreply.github.com>
2024-04-22 14:53:12 -0400

    outline commit

---
+++ components/Home.js
@@ -0,0 +1,14 @@
+// components/Home.js
+
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Welcome to the Casey's Home Screen!</Text>
+    </View>
+  );
+}
+
+export default Home;
