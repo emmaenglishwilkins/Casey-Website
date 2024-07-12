e767a26523c4e10f994cf6093fb2385bdfd57c0f
emmaenglishwilkins <ew1791@nyu.edu>
2024-07-12 17:52:38 -0400

    uniform draggable photos

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Home from './components/Home'; 
+import { View, StyleSheet } from 'react-native';
+import DraggablePhoto from './components/draggablePhoto';  // Adjust the import path accordingly
+
+import pic1 from './assets/pic1.png';
+import pic2 from './assets/pic2.png';
+import pic3 from './assets/pic3.png';
+import pic4 from './assets/pic4.png';
+import pic5 from './assets/pic5.png';
+import pic6 from './assets/pic6.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Home/>
+      <DraggablePhoto source={pic1} initialX={500} initialY={500} />
+      <DraggablePhoto source={pic2} initialX={400} initialY={500} />
+      <DraggablePhoto source={pic3} initialX={300} initialY={500} />
+      <DraggablePhoto source={pic4} initialX={200} initialY={500} />
+      <DraggablePhoto source={pic5} initialX={100} initialY={500} />
+      <DraggablePhoto source={pic6} initialX={0} initialY={500} />
       <StatusBar style="auto" />
     </View>
   );
@@ -19,4 +32,3 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
-
---
+++ components/draggablePhoto.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { View, Image, StyleSheet } from 'react-native';
+import Draggable from 'react-native-draggable';
+
+const DraggablePhoto = ({ source, initialX, initialY }) => {
+  const [position, setPosition] = useState({ x: initialX, y: initialY });
+
+  return (
+    <Draggable
+      x={position.x}
+      y={position.y}
+      onDragRelease={(event, gestureState, bounds) => {
+        setPosition({ x: bounds.left, y: bounds.top });
+      }}
+    >
+      <View style={styles.imageContainer}>
+        <Image source={source} style={styles.image} />
+      </View>
+    </Draggable>
+  );
+};
+
+const styles = StyleSheet.create({
+  imageContainer: {
+    width: 150,
+    height: 150,
+    marginVertical: 10,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+  },
+});
+
+export default DraggablePhoto;
