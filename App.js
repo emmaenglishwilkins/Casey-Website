c09411ce911c8364b23190b0180936250e061623
emmaenglishwilkins <78217522+emmaenglishwilkins@users.noreply.github.com>
2024-09-22 20:21:59 -0400

    new dog photos upload

--- App.js
+++ App.js
@@ -8,6 +8,11 @@ import pic3 from './assets/pic3.png';
 import pic4 from './assets/pic4.png';
 import pic5 from './assets/pic5.png';
 import pic6 from './assets/pic6.png';
+import pic7 from './assets/pic7.png';
+import pic8 from './assets/pic8.png';
+import pic9 from './assets/pic9.png';
+import pic10 from './assets/pic10.png';
+
 import DraggablePhoto from './components/draggablePhoto';
 
 export default function App() {
@@ -19,6 +24,10 @@ export default function App() {
       <DraggablePhoto source={pic4} initialX={400} initialY={500} />
       <DraggablePhoto source={pic5} initialX={500} initialY={600} />
       <DraggablePhoto source={pic6} initialX={0} initialY={500} />
+      <DraggablePhoto source={pic7} initialX={100} initialY={400} />
+      <DraggablePhoto source={pic8} initialX={200} initialY={300} />
+      <DraggablePhoto source={pic9} initialX={300} initialY={200} />
+      <DraggablePhoto source={pic10} initialX={400} initialY={100} />
     </View>
   );
 }
